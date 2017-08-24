b19d6f4435ca8320d7b31f7785df2c9957e3e545
xieyuxing <185246042@qq.com>
2017-08-24 17:21:22 +0800

    Initial Commit

---
+++ Hello Objective-C/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  Hello Objective-C
+//
+//  Created by  谢宇星 on 2017/8/24.
+//  Copyright © 2017年 xyx. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
